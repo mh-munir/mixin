d8ad4064004e24ad45dba55f8059538e9c560659
mh-munir <munir914794@gmail.com>
2022-11-01 00:40:06 +0600

    builder home page border remove

--- src/pages/BuilderHome.js
+++ src/pages/BuilderHome.js
@@ -12,12 +12,12 @@ const BuilderHome = () => {
         <section className='w-[94%] h-full bg-gray-light ml-auto relative py-16'>
             <BackLink className={"absolute top-5 left-5 text-xl font-bold text-gray-deep"} to={"/"}/>
             <BuilderHeader />
-            <div className='w-[70%] mr-40 mx-auto px-5'>
-                <div className='max-w-5xl ml-auto grid grid-cols-1-3 gap-8'>
-                  <div className='pt-16 w-full flex items-end'>
+            <div className='w-[70%] mr-40 mx-auto px-5 pt-16'>
+                <div className='max-w-5xl mx-auto flex gap-8 items-start'>
+                  <div className='w-full flex items-end'>
                     <BuilderArray />
                   </div>
-                  <div className='flex-auto flex items-end'>
+                  <div className='flex-auto min-h-[547px] min-w-[547px] border-2 border-gray-300 flex items-end'>
                     <img className='w-full h-auto object-cover' src={builderimg} alt="builder"/>
                   </div>
                 </div>
